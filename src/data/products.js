245640ff7e64ef84f8890622133f840a2ac92e56
Ifechukwu Aroh <aroh.freeman@gmail.com>
2024-07-30 21:16:31 -0400

    Create products.js
    Creation of products.js data for SpecialOffers.js

---
+++ src/data/products.js
@@ -0,0 +1,29 @@
+export const products = [
+    {
+      id: 1,
+      name: 'Espresso',
+      price: '$3.99',
+      image: '/images/scrollingbanner9.jpeg',
+    },
+    {
+      id: 2,
+      name: 'Americano',
+      price: '$4.29',
+      image: '/images/scrollingbanner3.jpeg',
+    },
+
+    {
+        id: 2,
+        name: 'Americano',
+        price: '$4.29',
+        image: '/images/scrollingbanner7.jpeg',
+      },
+
+      {
+        id: 2,
+        name: 'Americano',
+        price: '$4.29',
+        image: '/images/scrollingbanner8.jpeg',
+      },
+    // Add more products as needed
+  ];
