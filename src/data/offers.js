b27d2b94fd731989014449cfd3b6f225fa5c05ba
rajeshbegari <143718251+rajeshbegari@users.noreply.github.com>
2024-07-31 01:44:38 -0400

    Create offers.js
    Java Script files to display all the offers for the products in brew and bean

---
+++ src/data/offers.js
@@ -0,0 +1,40 @@
+export const offers = [
+    {
+      id: 1,
+      name: 'Latte',
+      price: '$4.99',
+      image: '/images/coffeetype.jpg',
+    },
+    {
+      id: 2,
+      name: 'Cappuccino',
+      price: '$5.49',
+      image: '/images/scrollingbanner6.jpeg',
+    },
+
+    {
+        id: 2,
+        name: 'Cappuccino',
+        price: '$5.49',
+        image: '/images/scrollingbanner10.jpeg',
+      },
+
+      {
+        id: 2,
+        name: 'Cappuccino',
+        price: '$5.49',
+        image: '/images/scrollingbanner2.jpeg',
+      },
+
+
+      {
+        id: 2,
+        name: 'Cappuccino',
+        price: '$5.49',
+        image: '/images/scrollingbanner4.jpeg',
+      },
+
+
+    
+    // Add more offers as needed
+  ];
