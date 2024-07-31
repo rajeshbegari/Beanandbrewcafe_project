72b8a3dc03d4a9d7376c25fbe1daa5a559743834
naveenreddy84 <nagulapallynaveen449@gmail.com>
2024-07-30 21:56:53 -0400

    Create feedback.js
    creating feedback.js file and pushing into github.

---
+++ src/data/feedback.js
@@ -0,0 +1,25 @@
+export const feedback = [
+    {
+      id: 1,
+      name: 'Naveen',
+      caption: 'Great coffee!',
+      description: 'I love the variety of coffees available at Bean and Brew Cafe. The service is excellent too!',
+      image: '/images/Customerfeedback2.jpeg',
+    },
+    {
+      id: 2,
+      name: 'Rajesh',
+      caption: 'Amazing ambiance!',
+      description: 'The ambiance at Bean and Brew Cafe is perfect for a relaxing coffee break. Highly recommend!',
+      image: '/images/Customerfeedback3.jpeg',
+    },
+
+    {
+        id: 2,
+        name: 'Ifechukwu Aroh',
+        caption: 'Amazing ambiance!',
+        description: 'The ambiance at Bean and Brew Cafe is perfect for a relaxing coffee break. Highly recommend!',
+        image: '/images/Customerfeedback1.jpeg',
+      },
+    // Add more feedback as needed
+  ];
