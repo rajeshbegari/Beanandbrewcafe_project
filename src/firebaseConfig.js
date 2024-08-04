866d3b0668ee0dea6b6401a86c3594b1cf6a0805
Ifechukwu Aroh <aroh.freeman@gmail.com>
2024-08-04 05:45:01 -0400

    Update firebaseConfig.js
    1. Integration of Firebase database and Storage for logged-in users' profile update 2. Update of Firebase rules to allow registered users to make changes to their account information

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -1,6 +1,7 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 
 const firebaseConfig = {
     apiKey: "AIzaSyCX65hJdBI2-ZR1SXlDqSK6Jpv_ocQfL_E",
@@ -16,5 +17,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
+const storage = getStorage(app);
 
-export { auth, db };
+export { auth, db, storage };
