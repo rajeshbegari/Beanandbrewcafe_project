b7e549f5b41b11023e4266d98fe2e30dbd582e40
naveenreddy84 <nagulapallynaveen449@gmail.com>
2024-08-14 22:34:08 -0400

    Update ShopContext.js
    updating the ShopContext page

--- src/context/ShopContext.js
+++ src/context/ShopContext.js
@@ -1,4 +1,3 @@
-
 import React, { createContext, useState } from 'react';
 
 export const ShopContext = createContext();
@@ -6,20 +5,43 @@ export const ShopContext = createContext();
 const ShopProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
   const [wishlist, setWishlist] = useState([]);
+  const [notification, setNotification] = useState('');
 
   const addToCart = (item) => {
-    setCart([...cart, item]);
+    const existingItem = cart.find(cartItem => cartItem.id === item.id);
+    if (existingItem) {
+      setCart(
+        cart.map(cartItem =>
+          cartItem.id === item.id
+            ? { ...cartItem, quantity: cartItem.quantity + 1 }
+            : cartItem
+        )
+      );
+      setNotification(`Increased quantity of ${item.name} in your cart.`);
+    } else {
+      setCart([...cart, { ...item, quantity: 1 }]);
+      setNotification(`Added ${item.name} to your cart.`);
+    }
   };
 
   const addToWishlist = (item) => {
-    setWishlist([...wishlist, item]);
+    if (!wishlist.some(wishlistItem => wishlistItem.id === item.id)) {
+      setWishlist([...wishlist, item]);
+      setNotification(`Added ${item.name} to your wishlist.`);
+    } else {
+      setNotification(`${item.name} is already in your wishlist.`);
+    }
+  };
+
+  const clearNotification = () => {
+    setNotification('');
   };
 
   return (
-    <ShopContext.Provider value={{ cart, wishlist, addToCart, addToWishlist }}>
+    <ShopContext.Provider value={{ cart, wishlist, addToCart, addToWishlist, notification, clearNotification }}>
       {children}
     </ShopContext.Provider>
   );
 };
 
-export default ShopProvider;
+export { ShopProvider };
