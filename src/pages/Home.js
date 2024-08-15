e74c5669cf2d5c771924e10e9d555f5a85cf67ce
naveenreddy84 <nagulapallynaveen449@gmail.com>
2024-08-14 22:47:22 -0400

    Update Home.js
    adding functionality for search results navigation

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,20 +1,26 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 import SearchBar from '../components/SearchBar';
 import SpecialOffers from '../components/SpecialOffers';
 import PopularProducts from '../components/PopularProducts';
 import CustomerFeedback from '../components/CustomerFeedback';
 import ScrollingBanner from '../components/ScrollingBanner';
-
+import '../styles.css';
 
 const Home = () => {
+  const navigate = useNavigate();
+
+  const handleSearch = (query) => {
+    navigate('/search-results', { state: { query } });
+  };
+
   return (
-    <div> 
-      <SearchBar />
+    <div className="home-page">
+      <SearchBar onSearch={handleSearch} />
       <SpecialOffers />
       <ScrollingBanner />
       <PopularProducts />
       <CustomerFeedback />
-      
     </div>
   );
 };
