7001b1846435f1a29fce97c4541994d93b223e10
naveenreddy84 <nagulapallynaveen449@gmail.com>
2024-08-14 22:36:46 -0400

    update PopularProducts.js and SpecialOffers.js
    updating the functions of the above pages.

--- src/components/PopularProducts.js
+++ src/components/PopularProducts.js
@@ -1,10 +1,19 @@
-import React, { useContext } from 'react';
+import React, { useContext, useEffect } from 'react';
 import { ShopContext } from '../context/ShopContext';
 import { products } from '../data/products';
 import PayPalButton from './PaypalButton';
 
 const PopularProducts = () => {
-  const { addToCart, addToWishlist } = useContext(ShopContext);
+  const { addToCart, addToWishlist, notification, clearNotification } = useContext(ShopContext);
+
+  useEffect(() => {
+    if (notification) {
+      const timer = setTimeout(() => {
+        clearNotification();
+      }, 2000);
+      return () => clearTimeout(timer);
+    }
+  }, [notification, clearNotification]);
 
   return (
     <div className="container">
@@ -15,14 +24,20 @@ const PopularProducts = () => {
             <img src={product.image} alt={product.name} />
             <h3>{product.name}</h3>
             <p>{product.price}</p>
-            <button onClick={() => addToCart(product)}>Add to Cart</button>
+            <button onClick={() => addToCart({ ...product, price: product.price })}>
+              Add to Cart
+            </button>
             <button onClick={() => addToWishlist(product)}>Add to Wishlist</button>
             <PayPalButton amount={product.price.replace('$', '')} />
           </div>
         ))}
       </div>
+      {notification && <div className="notification">{notification}</div>}
     </div>
   );
 };
 
 export default PopularProducts;
+
+
+
